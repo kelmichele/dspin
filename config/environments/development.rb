2a457a00e3c68cd0f66339a5c3f0d7cb2128cd0a
kelmichele <kelli@hsdesignhouse.com>
2019-02-14 10:57:35 -0600

    set up mailer info for requests

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,22 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'hsdesignhouse.com',
+    user_name:            Rails.application.credentials.gmail[:g_user],
+    password:             Rails.application.credentials.gmail[:g_pass],
+    authentication:       'plain',
+    enable_starttls_auto: true }
+
+    config.action_mailer.default_url_options = {
+    host: "hsdesignhouse.com"
+    }
+
+    config.action_mailer.default_options = {
+    from: "noreply@danaspinola.com"
+    }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
